6fae707660f2e6085514ee225abd3d6d2004fa7b
GageDeZoort <jgdezoort@gmail.com>
2020-07-26 16:42:39 -0400

    updated truth file calculations

--- analyses/truth/generate_truth_LP.py
+++ analyses/truth/generate_truth_LP.py
@@ -18,7 +18,7 @@ from trackml.dataset import load_dataset
 verbose = True
 parser = argparse.ArgumentParser('generate_truth_LP.py')
 add_arg = parser.add_argument
-add_arg('path', nargs='?', default='/home/sthais/data/sample')
+add_arg('path', nargs='?', default='/tigress/jdezoort/train_1')
 args = parser.parse_args()
 data_dir = args.path
 
@@ -27,25 +27,25 @@ files = [f for f in listdir(data_dir)]
 files = [f.split('.')[0] for f in files if "hits" in f]
 evt_ids = [f.split('-')[0] for f in files]
 
-outfile = open("truth_LP.txt", "w")
+endcaps = True
+outfile = open("truth_LP_EC.txt", "w")
 
 for evt_id in evt_ids:
     
     n_segs_per_graph = []
-    pt_cuts = [0.5, 0.6, 0.75, 1, 1.5, 2, 2.5, 3, 4, 5]
+    pt_cuts = [0.5, 0.6, 0.75, 1, 1.5, 2]
     
     for i in range(len(pt_cuts)):
 
         if (verbose): print(str(evt_id), str(pt_cuts[i]))
         
-        # TO-DO: full pixel detector
-        # pixel_layers = [(7,2), (7,4), (7,6), (7,8), (7,10), (7,12), (7,14),
-        #                 (8,2), (8,4), (8,6), (8,8),
-        #                 (9,2), (9,4), (9,6), (9,8), (9,10), (9,12), (9,14)]
-        
-        # pixel barrel layers
         pixel_layers = [(8,2), (8,4), (8,6), (8,8)]
-        
+        if (endcaps):
+            pixel_layers.extend([(7,2), (7,4), (7,6), (7,8),
+                                 (7,10), (7,12), (7,14),
+                                 (9,2), (9,4), (9,6), (9,8),
+                                 (9,10), (9,12), (9,14)])
+
         hits, cells, particles, truth = load_event(os.path.join(data_dir, evt_id))
         hits_by_loc = hits.groupby(['volume_id', 'layer_id'])
         hits = pd.concat([hits_by_loc.get_group(pixel_layers[i]).assign(layer=i)
@@ -68,19 +68,24 @@ for evt_id in evt_ids:
         ]
         
         particle_ids = hits.particle_id.unique()
-        segs_per_particle = [(hits[hits.particle_id == i].shape[0]-1) for i in particle_ids]
+        segs_per_particle = [(hits[hits.particle_id == i].shape[0]-1) 
+                             for i in particle_ids]
         n_segs = np.sum(segs_per_particle)
         n_segs_per_graph.append(n_segs)
         
-    outfile.write(str(evt_id) +" "+ str(n_segs_per_graph[0]) +" "+ str(n_segs_per_graph[1])  +" "+  
-                  str(n_segs_per_graph[2]) +" "+ str(n_segs_per_graph[3]) +" "+ str(n_segs_per_graph[4]) +" "+
-                  str(n_segs_per_graph[5]) +" "+ str(n_segs_per_graph[6]) +" "+ str(n_segs_per_graph[7]) +" "+
-                  str(n_segs_per_graph[8]) +" "+ str(n_segs_per_graph[9]) + "\n")
+    outfile.write("{0} {1} {2} {3} {4} {5} {6}\n"
+                  .format(str(evt_id), str(n_segs_per_graph[0]), 
+                          str(n_segs_per_graph[1]), str(n_segs_per_graph[2]), 
+                          str(n_segs_per_graph[3]), str(n_segs_per_graph[4]), 
+                          str(n_segs_per_graph[5])))
+    
     if (verbose):
-        print(str(evt_id) +" "+ str(n_segs_per_graph[0]) +" "+ str(n_segs_per_graph[1])  +" "+
-              str(n_segs_per_graph[2]) +" "+ str(n_segs_per_graph[3]) +" "+ str(n_segs_per_graph[4]) +" "+
-              str(n_segs_per_graph[5]) +" "+ str(n_segs_per_graph[6]) +" "+ str(n_segs_per_graph[7]) +" "+
-              str(n_segs_per_graph[8]) +" "+ str(n_segs_per_graph[9]) + "\n")
+        print("{0} {1} {2} {3} {4} {5} {6}\n"
+              .format(str(evt_id), str(n_segs_per_graph[0]),
+                      str(n_segs_per_graph[1]), str(n_segs_per_graph[2]),
+                      str(n_segs_per_graph[3]), str(n_segs_per_graph[4]),
+                      str(n_segs_per_graph[5])))
+
         
 outfile.close()
     
